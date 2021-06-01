bf6e3f324a059c0e03a2027fd063cb90fac746f5
Dane Enright <enrightco@comcast.net>
2021-06-01 17:50:16 -0600

    added comment and post models

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,36 @@
 const router = require('express').Router();
+const {} = require("../models");
+
+router.get('/', async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            // include: [
+            //     {
+
+            //     }
+            // ]
+        });
+
+        const blogs = blogData.map(() =>
+        blogs.get({ plain: true})
+        );
+        res.render('homepage', {
+            blogs,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err);
+    }
+});
+
+router.get('/blog/:id' async (req,res) => {
+    try { await
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
